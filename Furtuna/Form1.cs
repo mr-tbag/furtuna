92633d90f08e61531a8ec08fe58723491c3731d4
karim <450906k@gmail.com>
2022-08-12 11:47:32 +0500

    pipisca

---
+++ Furtuna/Form1.cs
@@ -0,0 +1,34 @@
+namespace Furtuna
+{
+    public partial class Form1 : Form
+    {
+        //private Tank _tank = new Tank(100,500,50,20,6,29,20);
+        private Game _game = new Game();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //var b = new Base(999);
+            
+            //b.Guns.Add(new Gun(6));
+            //b.Guns.Add(new Gun(8));
+
+            //var bullet = new Bullet(6, 100, 1000);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //_tank.Move(0.1f);
+            var g = panel1.CreateGraphics();
+            //g.Clear(Color.White);
+            //var brush = new SolidBrush(Color.Green);
+            //g.FillRectangle(brush, _tank.X, _tank.Y, _tank.Length, _tank.Width);
+            _game.Update(0.1f);
+            _game.Render(g);
+        }
+    }
+}
---
+++ Furtuna/Tank.cs
@@ -0,0 +1,103 @@
+﻿namespace Furtuna
+{
+    public class Game
+    {
+        public List<Tank> Tanks = new List<Tank>();
+        public Base Base = new Base(999);
+
+        public Game()
+        {
+            Tanks.Add(new Tank(100, 500, 100, 60, 3, 29, 90));
+            Tanks.Add(new Tank(100, 700, 100, 50, 5, 29, 50));
+        }
+
+        public void Update(float deltaSeconds)
+        {
+            foreach (var tank in Tanks)
+            {
+                tank.Move(deltaSeconds);
+            }
+        }
+
+        public void Render(Graphics g)
+        {
+            g.Clear(Color.White);
+            var brush = new SolidBrush(Color.Green);
+            foreach (var tank in Tanks)
+            {
+                g.FillRectangle(brush, tank.X, tank.Y, tank.Length, tank.Width);
+            }
+        }
+
+        public void Click(int x, int y)
+        {
+
+        }
+    }
+    public class Tank
+    {
+        public int X;
+        public int Y;
+        public int Length;
+        public int Width;
+        public int Hp;
+        public int Damage;
+        public int Speed;
+
+        public Tank(int x, int y, int length, int width, int hp, int damage, int speed)
+        {
+            X = x;
+            Y = y;
+            Length = length;
+            Width = width;
+            Hp = hp;
+            Damage = damage;
+            Speed = speed;
+        }
+
+        public void Move(float deltaSeconds)
+        {
+            X = X + (int)(Speed * deltaSeconds);
+        }
+    }
+
+    public class Base
+    {
+        public int X;
+        public List<Gun> Guns = new List<Gun>();
+
+        public Base(int x)
+        {
+            X = x;
+            
+        }
+    }
+
+    public class Gun
+    {
+        public int Damage;
+
+        public Gun(int damage)
+        {
+            Damage = damage;
+        }
+    }
+
+
+    public class Bullet
+    {
+        public int Damage;
+        public int X;
+        public int Y;
+
+        public Bullet(int damage, int x, int y)
+        {
+            Damage = damage;
+            X = x;
+            Y = y;
+        }
+
+    }
+    
+    
+}
